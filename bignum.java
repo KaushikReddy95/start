ed5caf3b189ca28c9bf3940405c9f37ce824c4c4
reddykaushik95 <reddykaushik95@gmail.com>
2025-01-24 18:34:02 +0530

    first commit

---
+++ bignum.java
@@ -0,0 +1,11 @@
+public class bignum{
+    public static void main(String[] args) {
+        int a=10;
+        for (int i = 1; i < 20; i++) {
+            if(i==a){
+                System.out.println("a is  found");
+            }
+            
+        }
+    }
+}
